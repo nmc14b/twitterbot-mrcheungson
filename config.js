abf6eee0cdcae62a8c9015e141d1b94a73699327
nmc14b <nmc14b@my.fsu.edu>
2018-08-16 12:18:05 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'Noa2SSewEiLyoLrTBtcJ9I6nh',
+  consumer_secret:      'Vfyah6M6z4HECLyFfd495l1zUvmr29KKv1we2iK1ET54GyBWCH',
+  access_token:         '1027738697909903361-5gQMPq1liLRqqCXHdgT9hJPoiOENcd',
+  access_token_secret:  'keoxdmyGhJo4Q5pMowz2qGEDoo7eDlxgBeIKLdt5VHsfH',
+}
